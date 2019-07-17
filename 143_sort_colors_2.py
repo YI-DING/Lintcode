39597e080228866fd3f87522406187d0ad486903
Ely Ding <yiding1@uchicago.edu>
2019-07-17 17:31:28 +0800

    1st commit

--- 143_sort_colors_2.py
+++ 143_sort_colors_2.py
@@ -1,23 +1,30 @@
+'''Given an array of n objects with k different colors (numbered from 1 to k), 
+sort them so that objects of the same color are adjacent, 
+with the colors in the order 1, 2, ... k.'''
 class Solution:
-    def sortColors(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
+    """
+    @param colors: A list of integer
+    @param k: An integer
+    @return: nothing
+    """
+    def sortColors2(self, colors, k):
         
         def quicksort(A, color, start):
-            end = len(A) - 1
-            while start <= end:
-                while start <= end and A[start] == color:
-                    start += 1
-                while start <= end and A[end] != color:
-                    end -= 1
-                if start <= end:
-                    A[start], A[end] = A[end], A[start]
-                    start += 1
-                    end -= 1
-            return start 
-        
-        if not nums:
-            return None 
-        start = quicksort(nums, 0, 0)
-        quicksort(nums, 1, start)
+        	end = len(A) - 1
+        	while start <= end:
+        		while start <= end and A[start] == color:
+        			start += 1
+        		while start <= end and A[end] != color:
+        			end -= 1
+        		if start <= end:
+        			A[start], A[end] = A[end], A[start]
+        			start += 1
+        			end -= 1
+        	return start 
+
+        if not colors or k == 1:
+        	return 
+        #k >= 2:
+        start = quicksort(colors, 1, 0)
+        for i in range(k-2):
+        	start = quicksort(colors, i+2, start)
