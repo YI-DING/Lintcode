586420546d874e5c5fec76d6f283b57b55a0da82
Ely Ding <yiding1@uchicago.edu>
2019-07-18 14:51:32 +0800

    1st commit

---
+++ 5_kth_largest_element.py
@@ -0,0 +1,8 @@
+class Solution:
+    """
+    @param n: An integer
+    @param nums: An array
+    @return: the Kth largest element
+    """
+    def kthLargestElement(self, n, nums):
+        # write your code here
